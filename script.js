d16d24ee19f5a8acab248a5fc8f66d5773826cc6
pekarsky.maxim@gmail.com <flufci@gmail.com>
2017-09-03 17:13:33 -0400

    add check for collisions

--- script.js
+++ script.js
@@ -8,20 +8,27 @@ var WIDTH = canvas.width;
 var HEIGHT = canvas.height;
 console.log("Canvas dimensions: " + HEIGHT + ", " + WIDTH);
 
+var laneWidth = (WIDTH-20)/3;
+var borderWidth = 10
+
 init();
 
 function init(){
     ctx = canvas.getContext("2d");
-    setInterval(draw, 50)
+    setInterval(draw, 20)
 }
 
 // test car:
 
-    var c1 = new Car(10, HEIGHT, 2, "red");
+    var c1 = new Car(2, 1);
 
-    var c2 = new Car(10, (HEIGHT + c1.height + 10), 1, "green");
+    var c2 = new Car(1.5, 2);
 
-    cars.push(c1, c2);
+    var c3 = new Car(1, 3);
+
+    var c4 = new Car(2, 3);
+
+    cars.push(c1, c2, c3, c4);
     console.log(cars);
 
 
@@ -38,19 +45,29 @@ function draw(){
 
     /* draw the track */
 
-    rect(0,                         0, 10, HEIGHT, "black");
-    rect(10,                        0, (WIDTH-20)/3, HEIGHT, "gray");
-    rect((10+(WIDTH-20)/3),         0, (WIDTH-20)/3, HEIGHT, "gray");
-    rect((10+((WIDTH-20)/3*2)),     0, (WIDTH-20)/3, HEIGHT, "gray");
-    rect((10+((WIDTH-20)/3*3)),     0, 10, HEIGHT, "black");
+    rect(0, 0, borderWidth, HEIGHT, "black");
+    rect(borderWidth, 0, laneWidth, HEIGHT, "gray");
+    rect((borderWidth+laneWidth), 0, laneWidth, HEIGHT, "gray");
+    rect((borderWidth+(laneWidth*2)), 0, laneWidth, HEIGHT, "gray");
+    rect((borderWidth+(laneWidth*3)), 0, borderWidth, HEIGHT, "black");
 
     /* draw cars: */
 
     cars.forEach(function(car){
 
-        rect((car.x + car.width), car.y, car.width, car.height, car.color);
-        console.log("Car position: [" + car.x + ", " + car.y + "]");
 
+        /* check for collision against every other car */
+
+
+        var color = "orange";
+
+        accident(car);              // check if this car is colliding with anyone (by cycling through every other car - wildly inefficient)
+
+        if(car.collided){
+            color = "red";
+        }
+
+        rect(car.x, car.y, car.width, car.height, color);
         car.y -= car.speed;
 
         if((car.y+car.height) < 0){
@@ -60,6 +77,52 @@ function draw(){
 
 }
 
+
+
+function accident(car){
+
+
+    cars.forEach(function(otherCar){
+
+        if( car.id != otherCar.id){
+            var xCollision = false;
+            var yCollision = false;
+
+            if(otherCar.x >= car.x && otherCar.x <= (car.x+car.width)){
+                xCollision = true;
+                console.log("xcol");
+            }
+
+            if((otherCar.x + otherCar.width) >= car.x && (otherCar.x + otherCar.width) <= (car.x+car.width)){
+                xCollision = true;
+                console.log("xcol");
+            }
+
+            if(otherCar.y >= car.y && otherCar.y <= (car.y+car.height)){
+                yCollision = true;
+                console.log("ycol");
+            }
+
+            if((otherCar.y+otherCar.height) >= car.y && (otherCar.y+otherCar.height) <= (car.y+car.height)){
+                yCollision = true;
+                console.log("ycol");
+            }
+
+            if(xCollision && yCollision){
+                console.log("COLLIDING!");
+                car.collided = true;
+                otherCar.collided = true;
+            } else {
+                car.collided = false;
+                otherCar.collided = false;
+            }
+        }
+
+    });
+
+    
+
+}
 
 
 // LIBRARY CODE
@@ -83,13 +146,24 @@ function rect(x,y,w,h, color) {
 }
  
     
-function Car(x, y, speed, color){
-    this.x = x,
-    this.y = y;
-    this.speed = speed;
+function Car(speed, lane){
+    this.id = Math.floor(Math.random()*1000)*Math.floor(Math.random()*1000);
     this.height = 10;
     this.width = 30;
-    this.color = color;
+
+    this.speed = speed;
+
+    if(lane != 1 && lane != 2 && lane != 3){
+        lane = 3;
+    }
+
+    this.x = (borderWidth + laneWidth*(lane-1) + (laneWidth - this.width)/2);
+    this.y = HEIGHT;
+
+    collided = "false";
+
+    this.skill = 1;                 // fow now, this'll control things like breaking
+
 }
 
 
