900b2653f5fa390d1b31e124dd3cc5485c2622c5
pekarsky.maxim@gmail.com <flufci@gmail.com>
2017-09-05 16:06:58 -0400

    add lane changes

--- script.js
+++ script.js
@@ -2,32 +2,38 @@ var canvas = document.getElementById("canvas");
 var ctx;
 
 var cars = [];
-
+var tempArray = [];                         // temporary array used to create cars
 
 var WIDTH = canvas.width;
 var HEIGHT = canvas.height;
 console.log("Canvas dimensions: " + HEIGHT + ", " + WIDTH);
 
-var laneWidth = (WIDTH-20)/3;
-var borderWidth = 10
+var numLanes = 5;
+var numberOfCars = 10;
 
-init();
+var laneWidth = (WIDTH-20)/numLanes;
+var borderWidth = 10;
+
+
+var carCounter = 0;
+
+ init();
 
 function init(){
     ctx = canvas.getContext("2d");
-    setInterval(draw, 10);
+    setInterval(draw, 20);
 }
 
 // test car:
 
-    var c1 = new Car(0.5, 3, 1);
-    var c2 = new Car(1, 2.5, 1);
-    var c3 = new Car(1, 0.5, 2);
-    var c4 = new Car(1, 1.5, 2);
-    var c5 = new Car(1, 2, 3);
-    var c6 = new Car(1, 1, 3);
 
-    cars.push(c1, c2, c3, c4, c5, c6);
+    
+
+    for(var i = 0; i < numberOfCars; i++){
+        var newCar = new Car(1, Math.floor(Math.random()*2.5)+0.5, (Math.floor(Math.random()*numLanes)+1));
+        tempArray.push(newCar);
+    }
+
     placeCars();
     console.log(cars);
 
@@ -48,10 +54,19 @@ function draw(){
     updateView();
 
     rect(0, 0, borderWidth, HEIGHT, "black");
-    rect(borderWidth, 0, laneWidth, HEIGHT, "gray");
-    rect((borderWidth+laneWidth), 0, laneWidth, HEIGHT, "gray");
-    rect((borderWidth+(laneWidth*2)), 0, laneWidth, HEIGHT, "gray");
-    rect((borderWidth+(laneWidth*3)), 0, borderWidth, HEIGHT, "black");
+
+
+    for(var i = 0; i < numLanes; i ++){
+        rect((borderWidth+laneWidth*i), 0, laneWidth, HEIGHT, "gray");
+    }
+
+
+
+
+    rect((borderWidth+(laneWidth*numLanes)), 0, borderWidth, HEIGHT, "black");
+
+
+
 
     /* draw cars: */
 
@@ -70,6 +85,7 @@ function draw(){
 
         if(car.lookAhead()){
             car.slowingDown = true;
+            car.frustration++;
         } else {
             car.slowingDown = false;
         }
@@ -83,9 +99,10 @@ function draw(){
         }
 
         rect(car.x, car.y, car.width, car.height, carColor);
-        circle((car.x + 5 ), (car.y + car.height - 6), 3, lightColor);
-        circle((car.x + car.width - 5 ), (car.y + car.height - 6), 3, lightColor);
-
+        circle((car.x + 0.25*car.width ), (car.y + 0.8*car.height), laneWidth/22, lightColor);
+        circle((car.x + 0.75*car.width), (car.y + 0.8*car.height), laneWidth/22, lightColor);
+        
+        text(car.id, (car.x+car.width/2) , car.y + 15);
 
                       // check if this car is colliding with anyone (by cycling through every other car - wildly inefficient)
         
@@ -107,23 +124,52 @@ function draw(){
             car.speed *= (1 + randBetween(-0.05, 0.05)*car.skill);
         }
 
+
+
+        
+        // to merge, the car should not be colliding, should be safe to merge,  should not be in the leftmost lane, and should not already be merging
+        if(!car.collided && car.safeToMerge() && car.lane > 1 && (car.lane == car.desiredLane)){
+            if(car.frustration > 30*car.skill){         // less skilled car should merge more often;
+                car.desiredLane--;                  // decrease lane by 1 => move left
+            }
+            
+        }
+
         if(car.speed > 3){
             car.speed = 3;
         }
 
 
-
-
-        car.y -= (car.speed);                             // this is the bit that makes the car move forward
-            
-
         // if we're off the top of the canvas, reposition to bottom of canvas
 
-       
+
         if((car.y + car.height) <= 0){
             car.y = (HEIGHT-car.height);
         }
 
+        if(car.y <= 0){                 // this creates a dummy car visual
+            var tempY = HEIGHT + car.y;
+            rect(car.x, tempY, car.width, car.height, carColor);
+            circle((car.x + 5 ), (tempY + car.height - 6), 3, lightColor);
+            circle((car.x + car.width - 5 ), (tempY + car.height - 6), 3, lightColor);
+            text(car.id, (car.x+car.width/2) , tempY + 15);
+        }
+
+
+        /*!! MOVE CAR !! */
+
+        car.y -= (car.speed);                             // this is the bit that makes the car move forward
+
+        if(car.desiredLane != car.lane && car.x > borderWidth){
+            car.x -= car.width/(20 *(1/car.skill))*(car.lane - car.desiredLane);            // merge based on skill (less skill - more abrupt merge);
+                                                                                              // move in the direction of the desired lane;
+
+            if((car.x + car.width/2) <= (borderWidth + (car.desiredLane-1)*laneWidth + laneWidth/2)){
+                car.lane = car.desiredLane;
+                car.frustration = 0;                        // reset frustration;
+            }
+
+        }
 
 
     });
@@ -131,51 +177,71 @@ function draw(){
 }
 
 function placeCars(){
-    console.log("later, we'll place cars randomly");
+
+    cars.push(tempArray[0]);
+    tempArray[0].y = HEIGHT - Math.random()*HEIGHT;                     // place the first car randomly
+
+    /* place second car until it doesn't collide */
+    if(tempArray.length > 1){
+        for(var i = 1; i < tempArray.length; i++){
+            var carPlaced = false;
+            while(!carPlaced){  
+                cars.push(tempArray[i]);
+                cars[cars.length-1].y = HEIGHT - Math.random()*HEIGHT;                // cars[cars.length-1] is the last element in the array
+                var collided = cars[cars.length-1].checkForCollision();
+                console.log("collided at placement: " + collided);
+                if(!collided){
+                    carPlaced = true;
+                    console.log("Car " + cars[cars.length-1].id + " has been placed!");
+                } else {
+                    cars.pop();                                      // remove last element (the colliding car);
+                }
+            }
+        }
+    }
+
+    ///
 }
+
+
 
 
     
 function Car(skill, speed, lane){
     
-    if(lane != 1 && lane != 2 && lane != 3){
-        lane = 3;
-    }
 
-    this.id = Math.floor(Math.random()*1000)*Math.floor(Math.random()*1000);
-    this.height = 35;
-    this.width = 20;
+
+    this.id = carCounter;
+    carCounter++;
+//    this.id = Math.floor(Math.random()*1000)*Math.floor(Math.random()*1000);
+    this.height = laneWidth/1.8;
+    this.width = laneWidth/3;
 
     this.speed = speed;
     this.desiredSpeed = speed;
 
     this.lane = lane;
-    this.nextLane = lane;
+    this.desiredLane = lane;
 
     this.x = (borderWidth + laneWidth*(lane-1) + (laneWidth - this.width)/2);
     this.y = HEIGHT - Math.random()*1000;
 
     this.collided = false;
     this.slowingDown = false;
-    this.skill = 1;                 // fow now, this'll control things like breaking
+    this.skill = skill;                 // fow now, this'll control things like breaking
+    this.frustration = 0;               // this will measure how many turns the car has spent driving slow;
 
 
     this.checkForCollision = function(){
 
         var self = this;
         var colliding = false;
-
-
         cars.forEach(function(otherCar){
 
             if(self.id != otherCar.id && self.lane == otherCar.lane){       // this is a problem, but I don't know why it doesn't work without the lane bit
                 var xCollision = false;
                 var yCollision = false;
-/*
-                console.log("self: " + self.x + ", " + self.y);
-                console.log("Other car: " + otherCar.x + ", " + otherCar.y);
 
-*/
                 if(self.x >= otherCar.x && self.x <= (otherCar.x + otherCar.width)){
                     xCollision = true;
                     
@@ -199,9 +265,12 @@ function Car(skill, speed, lane){
                 if(xCollision && yCollision){
                     self.collided = true;
                     colliding = true;
+                    self.collided = true;
+                //    console.log("colliding " + self.id);
                 } else {
                     self.collided = false;
                     colliding = false;
+                //    console.log("not colliding " + self.id);
                 }
             }
 
@@ -211,7 +280,27 @@ function Car(skill, speed, lane){
 
     }
 
-    this.lookAhead = function(){
+    this.checkForSpecificCollision = function(x, y){                // checks a coordinate for collision
+
+
+
+        var specificCollision = false;
+        var self = this;
+
+        cars.forEach(function(otherCar){
+
+            if(self.id != otherCar.id){       
+                if(x >= otherCar.x && x <= (otherCar.x + otherCar.width) && y >= otherCar.y && y <= (otherCar.y + otherCar.height)){
+                    specificCollision = true;
+                }
+            }
+
+        });
+
+        return specificCollision;
+    }
+
+    this.lookAhead = function(){                                    // looks a few cars ahead (based on skills) for collisions
 
         self = this;
         var slowingDown = false;
@@ -239,7 +328,6 @@ function Car(skill, speed, lane){
                 }
 
                 if(otherCar.y <= 0){                                 // edge case when the car is... literally on the edge of the map
-                    console.log("danger zone!" + otherCar.y);
                     safeDistance = (self.y - self.height*self.skill*2); 
                     if(safeDistance >= otherCar.y && safeDistance <= (otherCar.y+otherCar.height)){
                           
@@ -263,7 +351,83 @@ function Car(skill, speed, lane){
 
     }
 
+    this.safeToMerge = function() {
+
+        // check 8 points for merge:
+
+        /*
+            __
+            __  |    |
+            __  | [] |
+            __  |    |
+        */
+        var safeToChangeLanes = true;
+        
+
+        if(this.lane > 1){
+
+            var left = this.x - laneWidth;
+            var right = this.x - laneWidth + this.width;
+
+            /* draw the testing circles */
+            console.log(this.frustration);
+            if(this.frustration > 30){
+                var signalCircleRadius = laneWidth/22;
+                circle(left, this.y, signalCircleRadius, "black");
+                circle(right, this.y, signalCircleRadius, "black");
+                circle(left, this.y+this.height, signalCircleRadius, "black");
+                circle(right, this.y+this.height, signalCircleRadius, "black");
+                circle(left, this.y-this.height, signalCircleRadius, "black");
+                circle(right, this.y-this.height, signalCircleRadius, "black");
+                circle(left, this.y+this.height*2, signalCircleRadius, "black");
+                circle(right, this.y+this.height*2, signalCircleRadius, "black");
+            }
+
+            // front left
+            if(this.checkForSpecificCollision(left, this.y)){
+                safeToChangeLanes = false;
+            }
+            // front right
+            if(this.checkForSpecificCollision(right, this.y)){
+                safeToChangeLanes = false;
+            }
+
+            // back left
+            if(this.checkForSpecificCollision(left, this.y+this.height)){
+                safeToChangeLanes = false;
+            }
+            // back right
+            if(this.checkForSpecificCollision(right, this.y+this.height)){
+                safeToChangeLanes = false;
+            }
+
+            // one this ahead left
+            if(this.checkForSpecificCollision(left, this.y - this.height)){
+                safeToChangeLanes = false;
+            }
+            // one this ahead right
+            if(this.checkForSpecificCollision(right, this.y - this.height)){
+                safeToChangeLanes = false;
+            }
+
+            // one this behind left
+            if(this.checkForSpecificCollision(left, this.y+this.height*2)){
+                safeToChangeLanes = false;
+            }
+            // one this behind right
+            if(this.checkForSpecificCollision(right, this.y+this.height*2)){
+                safeToChangeLanes = false;
+            } 
+        } else {
+            safeToChangeLanes = false;
+        }
+
+        return safeToChangeLanes;
+
+    }
+
 }
+
 
 
 function clear() {
@@ -291,6 +455,13 @@ function rect(x,y,w,h, color) {
     ctx.stroke();
     ctx.fill();
 }
+
+function text(text, x, y){
+    ctx.font = "15px Arial";
+    ctx.fillStyle = "black";
+    ctx.textAlign = "center";
+    ctx.fillText(text, x, y);
+}
  
 
  
@@ -309,9 +480,10 @@ function updateView(){
     $("#car-info").empty();
 
     cars.forEach(function(car){
-    $("#car-info").append("<p> Car " + car.id + ", lane: " + car.lane + " is at <" + Math.floor(car.y) + "> traveling with the speed of " + Math.round(car.speed*100)/100 + ". Colliding: " + car.collided + "</p>")
+        $("#car-info").append("<p> Car " + car.id + ", lane: " + car.lane + " is at <" + Math.floor(car.y) + "> traveling with the speed of " + Math.round(car.speed*10)/10 + ". Frustration: " + car.frustration + ". Colliding: " + car.collided + "</p>")
+        $("#car-info").append(" <p>Lane: " + car.lane + ", desiredLane: " + car.desiredLane + "</p>")
 
-});
+    });
 }
 
 
